024df5ad0391c89675053f5f178c0383d4530da9
Christian Molkenthin <christian.molkenthin@uni-potsdam.de>
2023-01-30 19:42:00 +0100

    until gibbs sampler obj

---
+++ gpetas/utils/domain_class.py
@@ -0,0 +1,14 @@
+time_format = "%Y-%m-%d %H:%M:%S.%f"
+
+
+class region_class():
+    def __init__(self):
+        self.T_borders_all = None
+        self.T_borders_training = None
+        self.T_borders_testing = None
+        self.X_borders = None
+        self.X_borders_UTM_km = None
+        self.X_borders_original = None
+        self.time_origin = None
+        self.m0 = None
+        self.case_name = 'Rxxx'
