594b5b652fb75a0ac5aec1bbf04b24449de226ff
Christian Molkenthin <christian.molkenthin@uni-potsdam.de>
2023-03-18 13:50:32 +0100

    improved pred version

--- gpetas/core/prediction_1d.py
+++ gpetas/core/prediction_1d.py
@@ -2,26 +2,240 @@ import numpy as np
 from scipy.linalg import solve_triangular
 import scipy as sc
 import time
+import gpetas
 from gpetas.utils.some_fun import get_grid_data_for_a_point
 
+# some globals
+time_format = "%Y-%m-%d %H:%M:%S.%f"
+output_dir = "output_pred"
+output_dir_tables = "output_pred/tables"
+output_dir_figures = "output_pred/figures"
+output_dir_data = "output_pred/data"
 
 
-# stability issues
-def PHI_t_omori(c, p, t_start=0., t_end=np.inf):
-    PHI_t = np.nan
-    if (p > 1. and t_end == np.inf):
-        PHI_t = 1. / (p - 1) * (c + t_start) ** (1. - p)
-    if (p > 1. and t_end != np.inf):
-        PHI_t = 1. / (-p + 1) * ((c + t_end) ** (1. - p) - (c + t_start) ** (1. - p))
-    if (p == 1 and t_end != np.inf):
-        PHI_t = np.log(c + t_end) - np.log(c + t_start)
-    return (PHI_t)
+class predictions_1d_mle:
+    def __init__(self, mle_obj, tau1, tau2, tau0_Ht=0., Ksim=None, data_obj=None, seed=None, Bayesian_m_beta=None):
+        '''
+
+        :param mle_obj:
+        :type mle_obj:
+        :param tau1:
+        :type tau1:
+        :param tau2:
+        :type tau2:
+        :param tau0_Ht:
+        :type tau0_Ht:
+        :param Ksim:
+        :type Ksim:
+        :param data_obj:
+        :type data_obj:
+        :param seed:
+        :type seed:
+        :param Bayesian_m_beta:
+        :type Bayesian_m_beta:
+        '''
+        if Ksim is None:
+            Ksim = 1
+        dim = 1
+
+        self.mle_obj = mle_obj
+        self.tau0_Ht = tau0_Ht
+        self.tau1 = tau1
+        self.tau2 = tau2
+        self.tau_vec = np.array([tau0_Ht, tau1, tau2])
+        self.data_obj = mle_obj.data_obj
+        self.save_pred = None
+        self.N495_true = np.sum(self.data_obj.data_all.magnitudes[
+                                    np.logical_and(self.data_obj.data_all.times >= tau1,
+                                                   self.data_obj.data_all.times <= tau2)] >= 4.95)
+        self.Nm0_true = np.sum(self.data_obj.data_all.magnitudes[
+                                   np.logical_and(self.data_obj.data_all.times >= tau1,
+                                                  self.data_obj.data_all.times <= tau2)] >= self.data_obj.domain.m0)
+
+        self.seed = seed
+        if seed is not None:
+            np.random.seed(seed)
+            self.seed = seed
+        self.tic = time.perf_counter()
+
+        # offspring params
+        self.theta_Kcpadgq = np.copy(mle_obj.theta_mle_Kcpadgq)
+
+        # fixed params
+        self.m0 = self.data_obj.domain.m0
+        self.m_beta = mle_obj.m_beta_lower_T2
+        self.Bayesian_m_beta = Bayesian_m_beta
+        if data_obj is None:
+            data_obj = mle_obj.data_obj
+
+        init_save_dictionary(self)
+        self.save_pred['N495_true'] = np.copy(self.N495_true)
+        self.save_pred['Nm0_true'] = np.copy(self.Nm0_true)
+        self.save_pred['seed_orig'] = np.copy(seed)
+        self.save_pred['seed'] = np.copy(self.seed)
+        self.save_pred['data_obj'] = data_obj
+        self.save_pred['mle_obj'] = self.mle_obj
+        self.save_pred['m0'] = np.copy(self.data_obj.domain.m0)
+
+        # bg
+        mu = np.sum(mle_obj.mu_grid) * mle_obj.absX / len(mle_obj.X_grid)
+        N_0_Ksim = np.random.poisson(lam=mu * (tau2 - tau1), size=Ksim)
+
+        for k in range(Ksim):
+            N_0 = N_0_Ksim[k]
+            if N_0 == 0:
+                self.bgnew = np.zeros([int(len([])), 5])
+                self.bgnew_and_offspring = np.zeros([int(len([])), 5])
+            else:
+                # writing to a matrix
+                bg_events = np.zeros([N_0, 5])
+                bg_events[:, 0] = np.random.rand(N_0) * (tau2 - tau1)
+                bg_events[:, 1] = np.random.exponential(1. / self.m_beta,
+                                                        N_0) + self.m0  # m_i: marks
+                bg_events[:, 2] = None  # x_coord
+                bg_events[:, 3] = None  # y_coord
+                bg_events[:, 4] = np.zeros(N_0)  # branching z=0
+                sort_idx = np.argsort(bg_events[:, 0])
+                bg_events = bg_events[sort_idx, :]
+
+                # offspring (aftershocks) using: mle theta_phi
+                pred, pred_aug = sim_add_offspring(self, bg_events)
+                bgnew_and_offspring = np.copy(pred_aug)
+
+                # info 1: bg+off
+                self.bgnew = np.copy(bg_events)
+                self.bgnew[:, 0] += tau1
+                self.bgnew_and_offspring = np.copy(bgnew_and_offspring)
+                self.bgnew_and_offspring[:, 0] += tau1
+
+            # offspring from Ht
+            Ht_pred, Ht_pred_aug = sim_offspring_from_Ht(self)
+
+            # adding together: offspring from Ht + new background + offspring of background
+            if N_0 == 0:
+                added = np.copy(Ht_pred_aug)
+            else:
+                added = np.vstack([Ht_pred_aug, self.bgnew_and_offspring])
+            events_all_with_Ht_offspring = added[np.argsort(added[:, 0])]
+
+            # info 2: off_from_Ht
+            self.Ht_offspring = np.copy(Ht_pred_aug)
+            self.events_all_with_Ht_offspring = np.copy(events_all_with_Ht_offspring)
 
 
-def n(m_alpha, m_beta, K, c, p, t_start=0., t_end=np.inf):
-    n_t = PHI_t_omori(c, p, t_start, t_end)
-    n = n_t * K / (1. - m_alpha / m_beta)
-    return (n)
+            # save results
+            self.save_pred['pred_offspring_Ht'].append(self.Ht_offspring)
+            self.save_pred['pred_bgnew_and_offspring'].append(self.bgnew_and_offspring)
+            self.save_pred['pred_bgnew_and_offspring_with_Ht_offspring'].append(
+                self.events_all_with_Ht_offspring)
+            if len(self.events_all_with_Ht_offspring[:, 1]) > 0:
+                self.save_pred['M_max_all'].append(np.max(self.events_all_with_Ht_offspring[:, 1]))
+            self.save_pred['k_sample'].append(k)
+            self.save_pred['theta_Kcpadgq_k'].append(self.theta_Kcpadgq)
+            self.save_pred['N495_offspring_Ht'].append(np.sum(self.Ht_offspring[:, 1] >= 4.95))
+            self.save_pred['N495_all_with_Ht'].append(np.sum(self.events_all_with_Ht_offspring[:, 1] >= 4.95))
+            self.save_pred['N495_all_without_Ht'].append(np.sum(self.bgnew_and_offspring[:, 1] >= 4.95))
+            self.save_pred['N495_bgnew'].append(np.sum(self.bgnew[:, 1] >= 4.95))
+            self.save_pred['Nm0_offspring_Ht'].append(np.sum(self.Ht_offspring[:, 1] >= self.m0))
+            self.save_pred['Nm0_all_with_Ht'].append(np.sum(self.events_all_with_Ht_offspring[:, 1] >= self.m0))
+            self.save_pred['Nm0_all_without_Ht'].append(np.sum(self.bgnew_and_offspring[:, 1] >= self.m0))
+            self.save_pred['Nm0_bgnew'].append(np.sum(self.bgnew[:, 1] >= self.m0))
+            K, c, p, m_alpha = self.theta_Kcpadgq[:4]
+            m_beta = np.copy(self.m_beta)
+            self.n_stability = gpetas.utils.some_fun.n(m_alpha, m_beta, K, c, p, t_start=tau1, t_end=tau2)
+            self.n_stability_inf = gpetas.utils.some_fun.n(m_alpha, m_beta, K, c, p, t_start=0., t_end=np.inf)
+            self.save_pred['n_tau1_tau2'].append(self.n_stability)
+            self.save_pred['n_inf'].append(self.n_stability_inf)
+
+            if np.mod(k, 1) == 0:  # info every 10th event
+                tictoc = time.perf_counter() - self.tic
+                print('current simulation is k =', k + 1, 'of K =', Ksim,
+                      '%.2f sec. elapsed time.' % tictoc,
+                      ' approx. done %.1f percent.' % (100. * (k / float(Ksim))))
+
+            # some postprocessing
+            self.save_pred['cumsum'] = gpetas.prediction_2d.cumsum_events_pred(save_obj_pred=self.save_pred,
+                                                          tau1=self.tau1, tau2=self.tau2, m0=self.m0,
+                                                          which_events='all')
+
+
+def sim_add_offspring(obj, X0_events):
+    """
+    simulates offspring: with branching
+    :param X0_events:
+    :return:
+    """
+    tau0, tau1, tau2 = obj.tau_vec
+    T = tau2 - tau1
+    m_beta = obj.m_beta
+    m0 = obj.m0
+    K, c, p, m_alpha = obj.theta_Kcpadgq[:4]
+
+    x0 = X0_events[:, 0:4]
+    idx = 0
+    z_parent = np.zeros((np.size(x0[:, 0]), 1))
+    z_cluster = np.array(np.arange(np.size(x0[:, 0])) + 1)  # numbered clusters from 1,...,len(x)
+    z_generation = np.zeros((np.size(x0[:, 0]), 1))  # zeros
+    # lam_MOL = lambda t, m_i, K, c, p, m_alpha, m0: K * np.exp(m_alpha * (m_i - m0)) * 1. / (c + t) ** p
+    MOL_fun = lambda t, t_i, m_i, K, c, p, m_alpha, m0: K * np.exp(m_alpha * (m_i - m0)) * 1. / (c + t - t_i) ** p
+
+    x = x0
+    while idx < np.size(x[:, 0]):
+        # if np.mod(idx, 1000) == 0:  # info every 10th event
+        #    print('current event=', idx + 1)
+        t_i = x[idx, 0]
+        m_i = x[idx, 1]
+
+        fparams = np.array([t_i, m_i, K, c, p, m_alpha, m0])
+        max_lambda = MOL_fun(t_i, t_i, m_i, K, c, p, m_alpha, m0)
+        xnew = NHPPsim(t_i, tau2 - tau1, lambda_fun=MOL_fun, fargs=fparams, max_lambda=max_lambda, dim=1)
+        if xnew.size != 0:
+            Nnew = np.size(xnew)
+            id_cluster = z_cluster[idx]
+            id_generation = int(z_generation[idx])
+            if obj.Bayesian_m_beta is not None:
+                m_samples = sc.stats.lomax.rvs(size=np.size(xnew), c=obj.alpha_posterior_mb, loc=m0,
+                                               scale=obj.beta_posterior_mb)
+                xnew = np.vstack((xnew, m_samples)).transpose()
+            else:
+                xnew = np.vstack((xnew, np.random.exponential(1. / m_beta, np.size(xnew)) + m0)).transpose()
+            # empty xcoordinates
+            xnew = np.hstack((xnew, np.zeros([Nnew, 2])))
+            # update branching structure: z variables
+            z_parent = np.append(z_parent, idx * np.ones((Nnew, 1)) + 1)
+            z_cluster = np.append(z_cluster, id_cluster * np.ones((Nnew, 1)))
+            z_generation = np.append(z_generation, np.zeros((Nnew, 1)) + id_generation + 1.)
+
+            x = np.vstack((x, xnew))
+        idx = idx + 1
+
+    # sorting wrt time=x[:,0]
+    parents_sorted_t = np.zeros([x[:, 0].size, 1])
+    x_sorted_t = np.empty([x[:, 0].size, 4])
+    id_unsorted = np.arange(1, np.size(x[:, 0]) + 1)
+    idx = np.argsort(x[:, 0])
+    x_sorted_t[:, :] = x[idx, :]
+    z_parent_sorted_t = z_parent[idx]
+    z_cluster_sorted_t = z_cluster[idx]
+    z_generation_sorted_t = z_generation[idx]
+    # re-ordering from id_unsorted in time to sorted in time
+    for ii in z_parent_sorted_t[z_parent_sorted_t > 0]:
+        idz_pa = z_parent_sorted_t == ii
+        idd = id_unsorted[id_unsorted[idx] == ii]
+        parents_sorted_t[idz_pa] = idd
+
+    # generate data tmxyz
+    data_tmxyz = np.zeros((np.size(x_sorted_t[:, 0]), 5)) * np.nan
+    data_tmxyz[:, 0:4] = x_sorted_t
+    data_tmxyz[:, 4] = np.squeeze(parents_sorted_t).astype(int)
+    complete_branching_structure_tmxyzcg = np.zeros((np.size(x_sorted_t[:, 0]), 8)) * np.nan
+    complete_branching_structure_tmxyzcg[:, 0:5] = data_tmxyz[:, 0:5]
+    complete_branching_structure_tmxyzcg[:, 5] = z_cluster_sorted_t
+    complete_branching_structure_tmxyzcg[:, 6] = z_generation_sorted_t.reshape([-1, ])
+    complete_branching_structure_tmxyzcg[:, 7] = np.array(np.arange(np.size(x_sorted_t[:, 0])) + 1)
+
+    print('in add_offspring K:', K)
+    return data_tmxyz, complete_branching_structure_tmxyzcg
 
 
 def NHPPsim(tau1, tau2, lambda_fun, fargs, max_lambda, dim=1):
@@ -58,46 +272,124 @@ def init_save_dictionary(obj):
     obj.save_pred['tau_vec'].append(obj.tau_vec)
 
 
-class predictions_1d_mle:
-    def __init__(self, mle_obj, tau1, tau2, tau0_Ht=0., Ksim=None, data_obj=None, seed=None, Bayesian_m_beta=None):
-        '''
+def sim_offspring_from_Ht(obj, Ht=None, all_pts_yes=None, print_info=None):
+    """
 
-        :param mle_obj:
-        :type mle_obj:
-        :param tau1:
-        :type tau1:
-        :param tau2:
-        :type tau2:
-        :param tau0_Ht:
-        :type tau0_Ht:
-        :param Ksim:
-        :type Ksim:
-        :param data_obj:
-        :type data_obj:
-        :param seed:
-        :type seed:
-        :param Bayesian_m_beta:
-        :type Bayesian_m_beta:
-        '''
-        if Ksim is None:
-            Ksim = 1
-        dim = 1
+    :param theta_Kcpadgq:
+    :type theta_Kcpadgq:
+    :param spatial_offspring:
+    :type spatial_offspring:
+    :param Ht:
+    :type Ht:
+    :return:
+    :rtype:
+    """
 
-        self.tau0_Ht = tau0_Ht
-        self.tau1 = tau1
-        self.tau2 = tau2
-        self.tau_vec = np.array([tau0_Ht, tau1, tau2])
-        self.data_obj = mle_obj.data_obj
-        self.save_pred = None
-        self.N495_true = np.sum(self.data_obj.data_all.magnitudes[
-                                    np.logical_and(self.data_obj.data_all.times >= tau1,
-                                                   self.data_obj.data_all.times <= tau2)] >= 4.95)
-        self.Nm0_true = np.sum(self.data_obj.data_all.magnitudes[
-                                   np.logical_and(self.data_obj.data_all.times >= tau1,
-                                                  self.data_obj.data_all.times <= tau2)] >= self.data_obj.domain.m0)
+    tau0, tau1, tau2 = np.copy(obj.tau_vec)
+    if Ht is None:
+        idx = np.where((obj.data_obj.data_all.times >= tau0) & (obj.data_obj.data_all.times <= tau1))
+        Ht = np.empty([len(obj.data_obj.data_all.times[idx]), 4])
+        Ht[:, 0] = np.copy(obj.data_obj.data_all.times[idx])
+        Ht[:, 1] = np.copy(obj.data_obj.data_all.magnitudes[idx])
+        Ht[:, 2] = np.copy(obj.data_obj.data_all.positions[idx, 0])
+        Ht[:, 3] = np.copy(obj.data_obj.data_all.positions[idx, 1])
+    X0_events = np.copy(Ht)
+    obj.Ht = np.copy(Ht)
+    m_beta = np.copy(obj.m_beta)
+    m0 = np.copy(obj.m0)
+    K, c, p, m_alpha = obj.theta_Kcpadgq[:4]
 
-        self.seed = seed
-        if seed is not None:
-            np.random.seed(seed)
-            self.seed = seed
-        self.tic = time.perf_counter()
+    x0 = X0_events[:, 0:4]
+    if print_info is not None:
+        print('tau_vec              =', obj.tau_vec)
+        print('shape x0', x0.shape)
+    idx = 0
+    z_parent = np.zeros((np.size(x0[:, 0]), 1))
+    z_cluster = np.array(np.arange(np.size(x0[:, 0])) + 1)  # numbered clusters from 1,...,len(x)
+    # z_cluster = np.flipud(-np.array(np.arange(np.size(x0[:, 0])) + 1))  # numbered clusters from -N_Ht, ... -2, -1
+    z_generation = np.zeros((np.size(x0[:, 0]), 1))  # zeros
+    # lam_MOL = lambda t, m_i, K, c, p, m_alpha, m0: K * np.exp(m_alpha * (m_i - m0)) * 1. / (c + t) ** p
+    MOL_fun = lambda t, t_i, m_i, K, c, p, m_alpha, m0: K * np.exp(m_alpha * (m_i - m0)) * 1. / (c + t - t_i) ** p
+
+    x = x0
+    xnew = 0
+    while idx < np.size(x[:, 0]):
+        # if np.mod(idx, 1000) == 0:  # info every 10th event
+        #    print('current event=', idx + 1)
+        t_i = x[idx, 0]
+        m_i = x[idx, 1]
+
+        fparams = np.array([t_i, m_i, K, c, p, m_alpha, m0])
+        max_lambda = None
+        if t_i < tau1:
+            max_lambda = MOL_fun(tau1, t_i, m_i, K, c, p, m_alpha, m0)
+        if t_i >= tau1:
+            max_lambda = MOL_fun(t_i, t_i, m_i, K, c, p, m_alpha, m0)
+        # get new children
+        # xnew = obj.simNHHP(0., T - t_i, lam_MOL, fparams, max_lambda) + t_i
+        if t_i < tau1:
+            xnew = NHPPsim(tau1, tau2, lambda_fun=MOL_fun, fargs=fparams, max_lambda=max_lambda, dim=1)
+        if t_i >= tau1:
+            xnew = NHPPsim(t_i, tau2, lambda_fun=MOL_fun, fargs=fparams, max_lambda=max_lambda, dim=1)
+
+        if xnew.size != 0:
+            Nnew = np.size(xnew)
+            id_cluster = z_cluster[idx]
+            id_generation = int(z_generation[idx])
+            if obj.Bayesian_m_beta is not None:
+                m_samples = sc.stats.lomax.rvs(size=np.size(xnew), c=obj.alpha_posterior_mb, loc=m0,
+                                               scale=obj.beta_posterior_mb)
+                xnew = np.vstack((xnew, m_samples)).transpose()
+            else:
+                xnew = np.vstack((xnew, np.random.exponential(1. / m_beta, np.size(xnew)) + m0)).transpose()
+
+            # empty xcoordinates
+            xnew = np.hstack((xnew, np.zeros([Nnew, 2])))
+
+            # update branching structure: z variables
+            z_parent = np.append(z_parent, idx * np.ones((Nnew, 1)) + 1)
+            z_cluster = np.append(z_cluster, id_cluster * np.ones((Nnew, 1)))
+            z_generation = np.append(z_generation, np.zeros((Nnew, 1)) + id_generation + 1.)
+
+            x = np.vstack((x, xnew))
+
+        idx = idx + 1
+
+    # sorting wrt time=x[:,0]
+    parents_sorted_t = np.zeros([x[:, 0].size, 1])
+    x_sorted_t = np.empty([x[:, 0].size, 4])
+    z_parent_sorted_t = np.empty([x[:, 0].size, 1])
+    z_cluster_sorted_t = np.empty([x[:, 0].size, 1])
+    z_generation_sorted_t = np.empty([x[:, 0].size, 1])
+
+    id_unsorted = np.arange(1, np.size(x[:, 0]) + 1)
+    idx = np.argsort(x[:, 0])
+    x_sorted_t[:, :] = x[idx, :]
+    z_parent_sorted_t = z_parent[idx]
+    z_cluster_sorted_t = z_cluster[idx]
+    z_generation_sorted_t = z_generation[idx]
+    # re-ordering from id_unsorted in time to sorted in time
+    for ii in z_parent_sorted_t[z_parent_sorted_t > 0]:
+        idz_pa = z_parent_sorted_t == ii
+        idd = id_unsorted[id_unsorted[idx] == ii]
+        parents_sorted_t[idz_pa] = idd
+
+    # generate data tmxyz
+    shift_Ht = len(Ht)
+    data_tmxyz = np.zeros((np.size(x_sorted_t[:, 0]), 5)) * np.nan
+    data_tmxyz[:, 0:4] = x_sorted_t
+    data_tmxyz[:, 4] = np.squeeze(parents_sorted_t - shift_Ht).astype(int)
+    complete_branching_structure_tmxyzcg = np.zeros((np.size(x_sorted_t[:, 0]), 8)) * np.nan
+    complete_branching_structure_tmxyzcg[:, 0:5] = data_tmxyz[:, 0:5]
+    complete_branching_structure_tmxyzcg[:, 5] = z_cluster_sorted_t - shift_Ht
+    complete_branching_structure_tmxyzcg[:, 6] = z_generation_sorted_t.reshape([-1, ])
+    complete_branching_structure_tmxyzcg[:, 7] = -np.ones(
+        np.size(x_sorted_t[:, 0]))  # np.array(np.arange(np.size(x_sorted_t[:, 0])) + 1)
+    obj.Ht_offspring = np.copy(
+        complete_branching_structure_tmxyzcg[complete_branching_structure_tmxyzcg[:, 0] > tau1, :])
+    if all_pts_yes is not None:
+        obj.Ht_offspring = np.copy(complete_branching_structure_tmxyzcg)
+
+    print('in sim offspring from Ht K:', K)
+    return (data_tmxyz[complete_branching_structure_tmxyzcg[:, 0] > tau1, :],
+            complete_branching_structure_tmxyzcg[complete_branching_structure_tmxyzcg[:, 0] > tau1, :])
